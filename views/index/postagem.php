89ac310a4af36a6d60f6a1f4c33f2e1a89a08824
victorsouza02 <joao@joao>
2018-07-30 13:45:34 -0300

    Adição e Alteração de Fontes

--- views/index/postagem.php
+++ views/index/postagem.php
@@ -8,7 +8,7 @@ $dadosautor = $ctrl_user->dadosUsuarioById($dados['idUsuario']);
 <section id="postagem">
     <div class="row">
         <div class="col-12">
-            <h1 class="text-center titulo"> <?php echo $dados['titulo']; ?> </h1>
+            <h1 class="text-center titulo px-3 py-3"> <?php echo $dados['titulo']; ?> </h1>
         </div>
     </div>
     <div class="row">
