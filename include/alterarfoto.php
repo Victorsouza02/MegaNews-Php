8ef71b1c5da1d318769a75f7bebf1dacffd37389
Aluno <Aluno@DSN193915>
2018-07-30 21:19:36 -0300

    Corrigido Alterar Foto

--- include/alterarfoto.php
+++ include/alterarfoto.php
@@ -1,19 +1,22 @@
 <?php
 
-$path_foto = "/upload/foto_perfil/";
+$path_foto = "upload/foto_perfil/";
 if (isset($_POST['alterarfoto'])) {
     $foto = $_FILES['nova_foto'];
     $user = new Usuario();
     $resposta_foto = $user->validarFoto($foto);
 
     if ($resposta_foto == "") {
-        echo "<script> alert('oi'); </script>";
-        if (FOTOUSUARIO == "") {
-            unlink($path_foto . FOTOUSUARIO);
+    
+        if (FOTOUSUARIO != "") {
+            unlink($path_foto.FOTOUSUARIO);
         }
+        
         $user->setCodUsuario($_SESSION['idUsuario']);
         $user_ctrl = new CtrlUsuario();
         $user_ctrl->editarFoto($user);
+        
+        echo '<script>window.location.href = "index.php";</script>';
     } else {
         echo '<div class = "alert alert-danger">
             <button type = "button" class = "close" data-dismiss = "alert">&times;
